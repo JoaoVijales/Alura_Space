97c2b4b47a3bcc17cc735b47bb7ccf923d3bef1d
Schneider <vijales2000@gmail.com>
2023-02-10 18:20:54 -0300

    usando sqlite para guardar os dados e modificando a pagina de admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Fotografias(models.Model):
+    OPCAO_CATEGORIA =[
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta"),
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField( null=False, blank=False)
+    foto = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default='')
+
+    def __str__(self) -> str:
+        return f"fotografia [nome={self.nome}]"
