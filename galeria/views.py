6a5c9e586d99bd1f0e3b7fc6c762e6751286fcb3
Schneider <vijales2000@gmail.com>
2023-02-11 09:48:50 -0300

    ordenando fotos por data_foto

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografias
 
 def index(request):
-    fotografias = Fotografias.objects.filter(publicada=True)
+    fotografias = Fotografias.objects.order_by("data_foto").filter(publicada=True)
     dados = {
     1: {"nome":"Nebulosa de Carina",
         "legenda":"webbtelescope.org / NASA / James Webb"},
