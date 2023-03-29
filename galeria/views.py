b0ab27352a43b72038c5f6377c2726a8f80b8152
Schneider <vijales2000@gmail.com>
2023-03-29 14:10:28 -0300

    corrigindo requirements da aplicação

--- galeria/views.py
+++ galeria/views.py
@@ -9,3 +9,13 @@ def index(request):
 def imagem(request, foto_id):
     foto = get_object_or_404(Fotografias, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": foto})
+
+def buscar(request):
+    fotografias = Fotografias.objects.order_by("-dadta_foto").filter(publicada=True)
+
+    if buscar in request.GET:
+        nome_buscar  = request.GET['buscar']
+        if nome_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_buscar)
+
+    return render(request,'galeria/buscar.html', {"cards": fotografias})
